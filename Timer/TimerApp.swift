16ddc7041548d36354c3e3fd0e5e0f0858a44b4c
@Pablo735r <pabloamador8@hotmail.com>
2024-02-03 16:37:03 -0600

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Pablo Amador on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
